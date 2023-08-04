66825ba5deb0ca4988595e012bd220b855998aca
rully saputra <rully.saputra4@gmail.com>
2023-08-04 11:53:37 +0700

    feat: add title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Rully Saputra</title>
+      </Head>
       <body className="bg-sky-200/30">
         <Main />
         <NextScript />
