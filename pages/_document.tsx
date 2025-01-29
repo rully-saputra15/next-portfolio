2e1181f6e9a11924ccd47a3072ecd8cee5708155
rully <rully.saputra4@gmail.com>
2025-01-29 19:30:25 +0700

    fix: ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,11 +9,11 @@ export default function Document() {
         <meta name="description" content={metaBusinessOpportunityDescription} />
         <meta name="robots" content="index, follow" />
         <meta name="author" content="Rully Saputra" />
-        <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID || ""} />
       </Head>
       <body className="bg-[#faf9f7]">
         <Main />
         <NextScript />
+        <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID || ""} />
       </body>
     </Html>
   );
